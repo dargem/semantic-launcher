2eb03a886198981bb1548a2112f6f4c63c36678d
dargem <genericz145@gmail.com>
2026-09-03 09:43:13 +1000

    proper integrate usearch

---
+++ src/data/database.cpp
@@ -0,0 +1,13 @@
+#include "src/data/database.hpp"
+
+using unum::usearch::index_dense_t;
+using unum::usearch::metric_punned_t;
+using unum::usearch::metric_kind_t;
+using unum::usearch::scalar_kind_t;
+
+// Initialize the database, should read from all sources to collate files
+Database::Database() 
+    : m_vector_db(index_dense_t::make(metric_punned_t(3, metric_kind_t::l2sq_k, scalar_kind_t::f32_k)))
+{
+
+}
--- src/data/database.hpp
+++ src/data/database.hpp
@@ -3,18 +3,21 @@
 #include <vector>
 #include <cstddef>
 #include <usearch/index.hpp>
+#include <usearch/index_dense.hpp>
 #include "src/engine/result.hpp"
+#include <string_view>
 
 // Manages searchable files and provides 
-class Database {
+class Database
+{
 public:
     Database();
 
     // Uses exact / fuzzy match to get best n results, result size is <= n, sorted by strength
-    std::vector<Result> get_match_best(std::string_view str, size_t n) const; 
+    std::vector<Result> get_match_best(std::string_view query, size_t n) const; 
 
     // Uses semantic search to get best n results, result size is <= n, sorted by strength
-    std::vector<Result> get_semantic_best(std::string_view str, size_t num) const;
+    std::vector<Result> get_semantic_best(std::string_view query, size_t num) const;
 private:
-      
+      unum::usearch::index_dense_t m_vector_db;
 };
