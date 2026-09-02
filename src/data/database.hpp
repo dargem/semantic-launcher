58ccafa5f9b6c1f012560b65f50146d4e0aec3ea
dargem <genericz145@gmail.com>
2026-09-02 23:04:59 +1000

    plan out database which gets queried

---
+++ src/data/database.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <cstddef>
+
+#include "src/engine/result.hpp"
+
+// Manages searchable files and provides 
+class Database {
+public:
+    Database();
+
+    // Uses exact / fuzzy match to get best n results, result size is <= n, sorted by strength
+    std::vector<Result> get_match_best(std::string_view str, size_t n) const; 
+
+    // Uses semantic search to get best n results, result size is <= n, sorted by strength
+    std::vector<Result> get_semantic_best(std::string_view str, size_t num) const;
+private:
+};
--- src/engine/result.hpp
+++ src/engine/result.hpp
@@ -7,4 +7,5 @@ struct Result {
     std::string name;
     std::filesystem::path icon;
     std::filesystem::path executable;
+    double strength; // Strength of a result: [0, 1] 1 is ideal
 };
